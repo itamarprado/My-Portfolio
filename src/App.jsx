a8cda7b9e4e0ff0a0d4e31636e91bb94550d20dc
Itamar <itamar.tec01@hotmail.com>
2024-06-18 17:26:03 -0300

    feat: presentation section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Menu from "./components/Menu/Menu"
+import Presentation from "./components/Presentation/Presentation"
 
 const App = () => {
   return (
     <>
       <Menu />
+      <Presentation />
     </>
   )
 }
---
+++ src/components/Presentation/Presentation.jsx
@@ -0,0 +1,30 @@
+import { FaGithub, FaTelegram, FaDiscord } from "react-icons/fa";
+
+const Presentation = () => {
+  return (
+    <div className="bg-presentation bg-cover w-full min-h-80 relative">
+        {/* Gradient */}
+        <div className="flex flex-col w-full min-h-80 bg-opacity-75 bg-gradient-to-b from-transparent to-neutral-900 items-center justify-center">
+            {/* Title */}
+            <div className="flex flex-col w-3/4 self-center pb-5">
+                <h1 className="text-5xl font-bold self-start">
+                    Front-End
+                    <div className="w-14 h-1 bg-blue-300"></div>
+                </h1>
+                <h1 className="text-5xl font-bold self-end"><span className="text-blue-300">Dev</span>eloper</h1>
+            </div>
+            {/* Resume */}
+            <div className="font-light">
+                Improving your business
+            </div>
+            {/* Social media */}
+            <div className="flex absolute -bottom-8 gap-5 items-center">
+                    <FaGithub size={30}/>
+                    <FaTelegram size={30}/>
+                    <FaDiscord size={30}/>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Presentation
