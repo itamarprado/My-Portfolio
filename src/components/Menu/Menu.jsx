8a1b386aba2d153e170e9c8ecf7d01aa6f3a1287
Itamar <itamar.tec01@hotmail.com>
2024-06-18 17:24:50 -0300

    refactor: z-index and state

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,7 +3,7 @@ import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 
 const Menu = () => {
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
@@ -11,7 +11,7 @@ const Menu = () => {
     <div className="flex justify-between items-center px-5 relative bg-gradient-to-b from-gray-900 to-transparent">
         <div className="py-5">Hello, I&apos;m Itamar</div>
         <div className="cursor-pointer relative sm:hidden py-2" onClick={toggleMenu}>{isOpen ? <IoMdClose size={30}/> : <IoMenu size={30}/>}</div>
-        <nav className={`${isOpen ? "flex fixed" : "hidden"} pb-8 bg-neutral-900 absolute top-full left-0 flex-col w-full items-center sm:flex sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0`}>
+        <nav className={`${isOpen ? "flex" : "hidden"} z-50 pb-8 bg-neutral-900 absolute top-full left-0 flex-col w-full items-center sm:flex sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0`}>
           <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">About</a>
           <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">Projects</a>
           <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">Contact</a>
