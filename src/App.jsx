eed2bc1eaccc4a1ab0fe14c7bd3394606b56d966
Itamar <itamar.tec01@hotmail.com>
2024-06-20 10:59:58 -0300

    feat: added about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Menu from "./components/Menu/Menu"
 import Presentation from "./components/Presentation/Presentation"
 import Projects from "./components/Projects/Projects"
+import About from "./components/About/About"
 
 const App = () => {
   return (
     <>
       <Menu />
       <Presentation />
+      <About />
       <Projects />
     </>
   )
