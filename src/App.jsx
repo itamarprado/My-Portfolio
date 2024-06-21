9caa1bb3e28f1dd60170f1a14677a7b66be4714f
Itamar <itamar.tec01@hotmail.com>
2024-06-21 17:11:12 -0300

    refac: added skills and footer to app page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Menu from "./components/Menu/Menu"
 import Presentation from "./components/Presentation/Presentation"
 import Projects from "./components/Projects/Projects"
 import About from "./components/About/About"
+import Skills from "./components/Skills/Skills"
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
