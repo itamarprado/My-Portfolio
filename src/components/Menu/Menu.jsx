4b0c2473314c31eeb2c773de57484904ac5b222c
Itamar <itamar.tec01@hotmail.com>
2024-06-25 17:37:11 -0300

    refac: added max-width to menu and some responsive/style mod

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -8,14 +8,16 @@ const Menu = () => {
     setIsOpen(!isOpen)
   }
   return (
-    <div className="flex justify-between items-center px-5 relative bg-gradient-to-b from-gray-900 to-transparent">
+    <div className="flex justify-center items-center px-5 fixed z-50 bg-gradient-to-b from-gray-900 to-neutral-900 w-full">
+      <div className="flex justify-between w-full items-center max-w-[1200px]">
         <div className="py-5">Hello, I&apos;m Itamar</div>
         <div className="cursor-pointer relative sm:hidden py-2" onClick={toggleMenu}>{isOpen ? <IoMdClose size={30}/> : <IoMenu size={30}/>}</div>
-        <nav className={`${isOpen ? "flex" : "hidden"} z-50 pb-8 bg-neutral-900 absolute top-full left-0 flex-col w-full items-center sm:flex sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0`}>
+        <nav className={`${isOpen ? "flex" : "hidden"} z-50 pb-8 bg-neutral-900 absolute top-full left-0 flex-col w-full items-center sm:flex sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0 sm:pb-0 sm:bg-transparent`}>
           <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">About</a>
           <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">Projects</a>
           <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">Contact</a>
         </nav>
+      </div>
     </div>
   )
 }
