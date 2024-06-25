514a486c43b75a39c3b91961d8d031b8a9aa8661
Itamar <itamar.tec01@hotmail.com>
2024-06-25 17:35:28 -0300

    refac: restructured presentation with a new layout

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,26 +1,64 @@
-import { FaGithub, FaTelegram, FaDiscord } from "react-icons/fa";
+import { FaGithub, FaTelegram, FaDiscord, FaLinkedin } from "react-icons/fa";
+import { PiArrowRightLight } from "react-icons/pi";
 
 const Presentation = () => {
   return (
-    <div className="bg-presentation bg-cover w-full min-h-80 relative">
+    <div className="bg-presentation bg-cover w-full h-[100vh] relative bg-center">
         {/* Gradient */}
-        <div className="flex flex-col w-full min-h-80 bg-opacity-75 bg-gradient-to-b from-transparent to-neutral-900 items-center justify-center">
-            {/* Title */}
-            <div className="flex flex-col w-3/4 self-center pb-5">
-                <h1 className="text-5xl font-bold self-start w-max bg-gradient-to-r from-blue-700 via-blue-500-500 to-cyan-300 bg-[length:40%_6px] bg-left-bottom bg-no-repeat pb-[6px]">
-                    Front-End
-                </h1>
-                <h1 className="text-5xl font-bold self-end"><span className="text-blue-300">Dev</span>eloper</h1>
+        <div className="flex flex-col w-full h-[100vh] bg-gradient-to-b from-transparent to-neutral-900 items-center justify-center px-5">
+            {/* First section (Social, Presentation) */}
+            <div className="flex flex-col max-w-[1200px] md:flex-row items-center">
+                {/* Text content */}
+                <div className="md:flex-start md:max-w-[50%]">
+                    <div className="flex gap-5 items-start w-full pb-2">
+                            <FaGithub size={25} className="cursor-pointer hover:text-sky-500"/>
+                            <FaTelegram size={25} className="cursor-pointer hover:text-sky-500"/>
+                            <FaDiscord size={25} className="cursor-pointer hover:text-sky-500"/>
+                            <FaLinkedin size={25} className="cursor-pointer hover:text-sky-500"/>
+                    </div>
+                    <div className="flex flex-col w-full self-start pb-5 max-w-[360px]">
+                        <h1 className="text-4xl font-bold self-start w-max bg-gradient-to-r from-blue-700 via-blue-500 to-cyan-300 bg-[length:40%_3px] bg-left-bottom bg-no-repeat pb-[6px]">
+                            Front-end
+                        </h1>
+                        <h1 className="text-5xl font-bold bg-gradient-to-r from-sky-600 to-sky-200 bg-clip-text text-transparent">Developer</h1>
+                    </div>
+                    {/* Resume */}
+                    <div className="font-light self-start">
+                        <p>
+                            <strong>Welcome to my portfolio</strong>
+                        </p>
+                        I&apos;m Itamar, a front-end developer with focus on Web Development. I&apos;m building my career path by learning new technologies and building solo projects to showcase my skills.
+                    </div>
+
+                    <div className="flex gap-5 pt-5">
+                        <div className="flex items-center justify-center bg-gradient-to-br from-blue-700 to-sky-400 w-max rounded-lg p-[1px] hover:shadow-blue-700 hover:shadow-[0px_0px_25px_3px] duration-300">
+                            <button className="px-5 py-2 bg-neutral-800 w-[calc(100% - 1px)] h-[calc(100%-1px)] rounded-lg">
+                                Contact
+                            </button>
+                        </div>
+                        <button className="flex px-5 py-2 items-center justify-center bg-gradient-to-r from-blue-700 to-sky-400 rounded-lg hover:shadow-blue-700 hover:shadow-[0px_0px_15px_3px] hover:scale-110 duration-150">
+                            See projects
+                            <PiArrowRightLight size={25} className="ml-2"/>
+                        </button>
+                    </div>
+                </div>
+                {/* Img */}
+                {/* Future here will have an image with mockups of an project */}
             </div>
-            {/* Resume */}
-            <div className="font-light">
-                Improving your business
-            </div>
-            {/* Social media */}
-            <div className="flex absolute -bottom-3 gap-5 items-center">
-                    <FaGithub size={30}/>
-                    <FaTelegram size={30}/>
-                    <FaDiscord size={30}/>
+            {/* Arrow lines */}
+            <div className="flex flex-col w-full h-auto absolute items-center animate-bounce-slow bottom-10 md:bottom-0">
+                <span
+                className="bg-white w-12 h-5"
+                style={{
+                    clipPath: "polygon(3% 0, 50% 95%, 97% 0, 100% 0, 50% 100%, 0 0)",
+                }}
+                ></span>
+                <span
+                className="bg-white w-12 h-5 -mt-1"
+                style={{
+                    clipPath: "polygon(3% 0, 50% 95%, 97% 0, 100% 0, 50% 100%, 0 0)",
+                }}
+                ></span>
             </div>
         </div>
     </div>
