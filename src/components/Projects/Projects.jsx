21aea412a7acd744cc86bc655c15ae0596de888c
Itamar <itamar.tec01@hotmail.com>
2024-06-27 08:57:52 -0300

    refac: better responsiveness and adapted Project list to use map funcion with projectsData.js file and return the ProjectCard components with their data

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,32 +1,30 @@
-import { FaHtml5, FaCss3, FaReact, FaAngular, FaFigma } from "react-icons/fa";
+// import { FaHtml5, FaCss3, FaReact, FaAngular, FaFigma } from "react-icons/fa";
 import ProjectCard from "../ProjectCard/ProjectCard";
+import data from "../../data/projectsData";
 
 const Projects = () => {
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col px-5 pb-8">
-        <div className="w-[1px] h-12 mb-2 bg-white self-center"></div>
-        <div className="self-center">
-          <p className="text-sm leading-3 text-center">See my</p>
-          <h1 className="text-2xl">Projects</h1>
+    <div id="Projects" className="flex justify-center w-full px-5 pb-10">
+      <div className="flex flex-col w-full max-w-[1200px]">
+        <div className="flex flex-col px-5 pb-8">
+          <div className="w-[1px] h-12 mb-2 bg-white self-center"></div>
+          <div className="self-center">
+            <p className="text-sm leading-3 text-center">See my</p>
+            <h1 className="text-2xl">Projects</h1>
+          </div>
+        </div>
+        <div className="flex flex-wrap">
+          {data.map((project) => {
+            return <ProjectCard
+                    title={project.title}
+                    desc={project.desc}
+                    skills={project.skills}
+                    imgUrl={project.imgUrl}
+                    key={project.id}
+                    />
+          })}
         </div>
       </div>
-      <ProjectCard
-        title="Portfolio"
-        desc="This project is my portfolio to show my projects and my knowledge to the public!"
-        skills={[<FaHtml5 key={0} />, <FaCss3 key={1} />, <FaReact key={2} />]}
-        imgUrl={
-          "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/10/Como-Fazer-Um-Portfolio-Digital-1.webp"
-        }
-      />
-      <ProjectCard
-        title="E-commerce"
-        desc="An e-commerce"
-        skills={[<FaHtml5 key={0} />, <FaFigma key={1} />, <FaAngular key={2} />]}
-        imgUrl={
-          "https://blog.ideiamais.com.br/wp-content/uploads/2019/03/processo-ecommerce-750x410.jpg"
-        }
-      />
     </div>
   );
 };
