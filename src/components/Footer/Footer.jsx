aa993c334cf43fb25458b88825214c891efa4989
Itamar <itamarpj00@hotmail.com>
2024-06-27 15:25:46 -0300

    refac: restructured footer, more minimalist

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,29 +2,32 @@ import { FaGithub, FaTelegram, FaDiscord, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center w-full bg-gradient-to-t from-neutral-800 to-neutral-900 border-t-[1px] border-neutral-700 px-5 py-3">
-      <div className="pb-5">
-        <p className="font-light text-md pb-5">Follow me on me social media!</p>
-        <div className="flex gap-2 text-3xl justify-center">
-          <div className="p-2 bg-neutral-500 rounded-xl cursor-pointer hover:bg-sky-300 hover:text-neutral-900 duration-300">
-            <FaGithub />
-          </div>
-          <div className="p-2 bg-neutral-500 rounded-xl cursor-pointer hover:bg-sky-300 hover:text-neutral-900 duration-300">
-            <FaTelegram />
-          </div>
-          <div className="p-2 bg-neutral-500 rounded-xl cursor-pointer hover:bg-sky-300 hover:text-neutral-900 duration-300">
-            <FaDiscord />
-          </div>
-          <div className="p-2 bg-neutral-500 rounded-xl cursor-pointer hover:bg-sky-300 hover:text-neutral-900 duration-300">
-            <FaLinkedin />
+    <div className="flex flex-col items-center w-full bg-gradient-to-t from-neutral-800 to-neutral-900 border-t-[1px] border-neutral-700 px-5 py-10">
+      <div className="flex flex-col gap-5 w-full max-w-[1200px] justify-between sm:flex-row">
+        <div>
+          <h1 className="text-2xl font-semibold pb-10 bg-gradient-to-r from-sky-500 to-sky-300 bg-clip-text text-transparent">
+            Nice to see you here!
+          </h1>
+          <div className="flex flex-col">
+            <p className="font-medium text-sm text-neutral-300">Made with love &copy; 2024 Itamar do Prado</p>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <p className="font-light text-sm">Developed with love (and coffee) by <span className="text-blue-400">Itamar</span></p>
-        <p className="font-light text-sm">
-          &copy; All rights reserved
-        </p>
+        <div className="flex items-end">
+          <div className="flex gap-2 text-2xl justify-center">
+            <a href="https://github.com/itamarprado" target="_blank" className="p-2 bg-neutral-700 rounded-xl cursor-pointer from-sky-300 to-sky-700 hover:bg-gradient-to-br hover: hover:text-neutral-900 duration-300">
+              <FaGithub />
+            </a>
+            <a href="" target="_blank" className="p-2 bg-neutral-700 rounded-xl cursor-pointer from-sky-300 to-sky-700 hover:bg-gradient-to-br hover: hover:text-neutral-900 duration-300">
+              <FaTelegram />
+            </a>
+            <a href="https://discord.gg/HAsTvyBr" target="_blank" className="p-2 bg-neutral-700 rounded-xl cursor-pointer from-sky-300 to-sky-700 hover:bg-gradient-to-br hover: hover:text-neutral-900 duration-300">
+              <FaDiscord />
+            </a>
+            <a href="https://www.linkedin.com/in/itamarprado/" target="_blank" className="p-2 bg-neutral-700 rounded-xl cursor-pointer from-sky-300 to-sky-700 hover:bg-gradient-to-br hover: hover:text-neutral-900 duration-300">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
