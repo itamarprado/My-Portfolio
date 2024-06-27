fe2e302250b5e557e05e0e8140d6711c6e531b19
Itamar <itamar.tec01@hotmail.com>
2024-06-27 08:50:22 -0300

    refac: transformed project data into objects in JS file

---
+++ src/data/projectsData.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    id: 1,
+    title: "Portfolio",
+    desc: "This project is my portfolio to show my projects and my knowledge to the public!",
+    skills: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    ],
+    imgUrl:
+      "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/10/Como-Fazer-Um-Portfolio-Digital-1.webp",
+  },
+  {
+    id: 2,
+    title: "Ecommerce",
+    desc: "An Ecommerce website to show my skills developing CRUDs and ERPs!",
+    skills: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    ],
+    imgUrl:
+      "https://blog.ideiamais.com.br/wp-content/uploads/2019/03/processo-ecommerce-750x410.jpg",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    desc: "This project is my portfolio to show my projects and my knowledge to the public!",
+    skills: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    ],
+    imgUrl:
+      "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/10/Como-Fazer-Um-Portfolio-Digital-1.webp",
+  },
+];
+
+export default data;
