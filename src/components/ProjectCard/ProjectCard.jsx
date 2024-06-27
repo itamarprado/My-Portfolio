95a50e9e409c4365cdf06ace62bf89995b55b16d
Itamar <itamar.tec01@hotmail.com>
2024-06-27 08:56:21 -0300

    refac: better responsiveness and adapted to use list of skills with map function

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,17 +1,21 @@
 const ProjectCard = ({title, desc, skills, imgUrl}) => {
   return (
-    <div className="flex flex-col items-centes w-full px-5 mb-10">
-        <div className="w-full">
-            <img src={imgUrl} alt="" />
+    <div className="flex flex-col w-full sm:w-1/2 lg:w-1/3  p-4">
+        <div className="w-full h-52">
+            <img src={imgUrl} alt="" className="w-full h-full object-cover"/>
         </div>
-        <div className="flex text-2xl justify-between text-blue-300 pb-1">
-            {title}
-        </div>
-        <div className="flex text-2xl pb-1">
-            {skills}
-        </div>
-        <div className="font-light">
-            {desc}
+        <div className="h-auto">
+            <div className="flex text-2xl justify-between text-blue-300 pb-1">
+                {title}
+            </div>
+            <div className="flex text-2xl pb-1 gap-2">
+                {skills.map((skill, index) => {
+                    return <img src={skill} className="max-w-6" key={index}/>
+                })}
+            </div>
+            <div className="font-light">
+                {desc}
+            </div>
         </div>
     </div>
   )
