6763e05db4bed79f8a4b4ea3742ded09314b1dc3
Itamar <itamar.tec01@hotmail.com>
2024-06-27 10:09:02 -0300

    refac: I changed project data and added more projects to see the responsiveness behavior

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,8 +1,8 @@
 const data = [
   {
     id: 1,
-    title: "Portfolio",
-    desc: "This project is my portfolio to show my projects and my knowledge to the public!",
+    title: "Project",
+    desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
     skills: [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
@@ -10,12 +10,12 @@ const data = [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
     ],
     imgUrl:
-      "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/10/Como-Fazer-Um-Portfolio-Digital-1.webp",
+      "https://cdn.prod.website-files.com/63c5e29f1b5bc83fe0af2489/6424d753f8eb7a9e69c372fc_Gantt%20Chart%20Online%20Software%20Instagantt%20Ideation%202.webp",
   },
   {
     id: 2,
-    title: "Ecommerce",
-    desc: "An Ecommerce website to show my skills developing CRUDs and ERPs!",
+    title: "Project",
+    desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
     skills: [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
@@ -23,12 +23,12 @@ const data = [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
     ],
     imgUrl:
-      "https://blog.ideiamais.com.br/wp-content/uploads/2019/03/processo-ecommerce-750x410.jpg",
+      "https://wcs.smartdraw.com/working-smarter/img/how-to-create-a-project-planning-map.svg?bn=15100111938",
   },
   {
     id: 3,
-    title: "Portfolio",
-    desc: "This project is my portfolio to show my projects and my knowledge to the public!",
+    title: "Project",
+    desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
     skills: [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
@@ -36,7 +36,33 @@ const data = [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
     ],
     imgUrl:
-      "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/10/Como-Fazer-Um-Portfolio-Digital-1.webp",
+      "https://cdn.sanity.io/images/tlr8oxjg/production/d63904e726e42ae2491c29106065e9762dbcccde-1456x816.png?w=3840&q=80&fit=clip&auto=format",
+  },
+  {
+    id: 4,
+    title: "Project",
+    desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
+    skills: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    ],
+    imgUrl:
+      "https://www.projectmanager.com/wp-content/uploads/2022/09/Project-Management.png",
+  },
+  {
+    id: 5,
+    title: "Project",
+    desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
+    skills: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    ],
+    imgUrl:
+      "https://www.elmhurst.edu/wp-content/uploads/2018/12/5-skills-project-management-degree-elmhurst-college-infographic-thumb.jpg",
   },
 ];
 
