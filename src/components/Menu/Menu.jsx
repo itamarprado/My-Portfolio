cc2806a1811a67a507fa157f1798fbe654a369a2
Itamar <itamar.tec01@hotmail.com>
2024-06-27 08:43:25 -0300

    feat: added Link to scroll through the page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
+import { Link } from "react-scroll";
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -13,9 +14,19 @@ const Menu = () => {
         <div className="py-5">Hello, I&apos;m Itamar</div>
         <div className="cursor-pointer relative sm:hidden py-2" onClick={toggleMenu}>{isOpen ? <IoMdClose size={30}/> : <IoMenu size={30}/>}</div>
         <nav className={`${isOpen ? "flex" : "hidden"} z-50 pb-8 bg-neutral-900 absolute top-full left-0 flex-col w-full items-center sm:flex sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0 sm:pb-0 sm:bg-transparent`}>
-          <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">About</a>
-          <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">Projects</a>
-          <a className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">Contact</a>
+          <Link to="About" spy={true} smooth={true} duration={1000} offset={-64} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+            About
+          </Link>
+          <Link to="Skills" spy={true} smooth={true} duration={1000} offset={-64} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+            Skills
+          </Link>
+          <Link to="Projects" spy={true} smooth={true} duration={1000} offset={-64} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+            Projects
+          </Link>
+          <Link to="Contact" spy={true} smooth={true} duration={1000} offset={-64} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+            Contact
+          </Link>
+
         </nav>
       </div>
     </div>
