4bc6f0910fc0c65d0a7d34d16138a4c99fb4525e
Itamar <itamarpj00@hotmail.com>
2024-06-28 11:59:56 -0300

    feat: added social media links to an object to make it easier if there are changes

---
+++ src/data/socialMedia.js
@@ -0,0 +1,8 @@
+const socialMedia = {
+  "Github": "https://github.com/itamarprado",
+  "Telegram": "https://t.me/itamarprado",
+  "Discord": "https://discord.gg/HAsTvyBr",
+  "Linkedin": "https://www.linkedin.com/in/itamarprado/"
+};
+
+export default socialMedia;
