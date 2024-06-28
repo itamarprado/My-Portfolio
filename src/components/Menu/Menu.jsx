9a4a7245fad46cb1a77d3013ea2bb6106e9e72c2
praad0o <itamarpj00@hotmail.com>
2024-06-27 23:09:06 -0300

    fix: code was not working fine with the change of mobile/desktop menu - the variable to control the state was not properly used

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,18 +2,23 @@ import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import { Link } from "react-scroll";
+import useSize from "../../hooks/useSize"
 
 const Menu = () => {
+  // Get window size
+  const windowSize = useSize()
+  // Manage the mobile menu
   const [isOpen, setIsOpen] = useState(false)
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
+
   return (
     <div className="flex justify-center items-center px-5 fixed z-50 bg-gradient-to-b from-neutral-900 to-midnight w-full">
       <div className="flex justify-between w-full items-center max-w-6xl">
         <div className="py-5">Hey, I&apos;m Itamar</div>
         <div className="cursor-pointer relative sm:hidden py-2" onClick={toggleMenu}>{isOpen ? <IoMdClose size={30}/> : <IoMenu size={30}/>}</div>
-        <nav className={`${isOpen ? "flex" : "hidden"} z-50 pb-8 bg-midnight absolute top-full left-0 flex-col w-full items-center sm:flex sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0 sm:pb-0 sm:bg-transparent`}>
+        <nav className={`${windowSize[0]<640 ? (isOpen ? "flex" : "hidden") : "flex"} z-50 pb-8 bg-midnight absolute top-full left-0 flex-col w-full items-center sm:flex sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0 sm:pb-0 sm:bg-transparent`}>
           <Link to="About" spy={true} smooth={true} duration={1000} offset={-64} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
             About
           </Link>
@@ -26,7 +31,7 @@ const Menu = () => {
           <Link to="Contact" spy={true} smooth={true} duration={1000} offset={-64} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
             Contact
           </Link>
-
+          
         </nav>
       </div>
     </div>
