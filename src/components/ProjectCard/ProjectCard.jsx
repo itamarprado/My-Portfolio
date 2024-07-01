b88da8339b069083a193e428e11b646874c6ccbe
Itamar <itamarpj00@hotmail.com>
2024-07-01 13:12:02 -0300

    refac: restructured the code to use the new projectsData structure

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,23 +1,24 @@
-const ProjectCard = ({title, desc, skills, imgUrl}) => {
+const ProjectCard = ({ title, desc, skills, imgUrl }) => {
+  const skillsUsed = [];
+  skills.map((skill) => skillsUsed.push([skill.id, skill.icon]));
+
   return (
-    <div className="flex flex-col w-full sm:w-1/2 lg:w-1/3  p-4">
-        <div className="w-full h-52">
-            <img src={imgUrl} alt="" className="w-full h-full object-cover"/>
+    <div className="flex flex-col w-full sm:w-1/2 lg:w-1/3 p-4 ">
+      <div className="w-full h-52 opacity-70 cursor-pointer duration-300 hover:scale-[103%] hover:opacity-100">
+        <img src={imgUrl} alt="" className="w-full h-full object-cover" />
+      </div>
+      <div className="h-auto">
+        <div className="flex text-2xl justify-between text-blue-300 pb-1 cursor-pointer w-max">
+          {title}
         </div>
-        <div className="h-auto">
-            <div className="flex text-2xl justify-between text-blue-300 pb-1">
-                {title}
-            </div>
-            <div className="flex text-2xl pb-1 gap-2">
-                {skills.map((skill, index) => {
-                    return <img src={skill} className="max-w-6" key={index}/>
-                })}
-            </div>
-            <div className="font-light">
-                {desc}
-            </div>
+        <div className="flex text-2xl pb-1 gap-2">
+          {skillsUsed.map((icon) => {
+            return <img src={icon[1]} className="max-w-6" key={icon[0]} />;
+          })}
         </div>
+        <div className="font-light">{desc}</div>
+      </div>
     </div>
-  )
-}
-export default ProjectCard
+  );
+};
+export default ProjectCard;
