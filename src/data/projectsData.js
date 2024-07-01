db6cf327538ad0d766045faec5499d2d1d33ec44
Itamar <itamarpj00@hotmail.com>
2024-07-01 12:48:15 -0300

    refac: I restructured the projectsData

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,68 +1,303 @@
 const data = [
   {
     id: 1,
-    title: "Project",
-    desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
+    title: "Personal Portfolio",
+    desc: "This is my personal portfolio developed by me to expose all my works to the public. I tried to make it minimalist, easy comprehension and with my favourite color, which is blue",
     skills: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      {
+        id: 100,
+        skillName: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      },
+      {
+        id: 101,
+        skillName: "Javascript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        id: 102,
+        skillName: "HTML",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      },
+      {
+        id: 103,
+        skillName: "Tailwind CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      },
+      {
+        id: 104,
+        skillName: "Vite",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+      },
     ],
     imgUrl:
       "https://cdn.prod.website-files.com/63c5e29f1b5bc83fe0af2489/6424d753f8eb7a9e69c372fc_Gantt%20Chart%20Online%20Software%20Instagantt%20Ideation%202.webp",
+    gitHubUrl: "https://github.com/itamarprado/my-portfolio",
+    webUrl: "",
+    detailedInfo: {
+      myRole: "Full Stack",
+      projectType: "Personal Project",
+      sections: {
+        overview: {
+          objective: {
+            title: "Objective",
+            content:
+              "Looking for the need to expose my projects and prove my knowledge, the principal objective of this project is to expose my works to the public, proving that I can develop beautiful projects, wireframes, websites etc.",
+          },
+          targetAudience: {
+            title: "Target Audience",
+            content:
+              "Anyone who wants to see my work, contact-me or even need inspiration to build their own projects.",
+          },
+        },
+        design: {
+          wireframe: {
+            title: "Wireframe",
+            img: "https://uizard.io/static/dd6752e6face7dd5fa64599cbc7dbb48/0cfa7/27e2cf1da349fbe6cf7d3452cd5bfdc7f557bf7b-1440x835.webp",
+          },
+        },
+      },
+    },
   },
   {
     id: 2,
-    title: "Project",
+    title: "Project 2",
     desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
     skills: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      {
+        id: 100,
+        skillName: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      },
+      {
+        id: 101,
+        skillName: "Javascript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        id: 102,
+        skillName: "HTML",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      },
+      {
+        id: 103,
+        skillName: "Tailwind CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      },
+      {
+        id: 104,
+        skillName: "Vite",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+      },
     ],
     imgUrl:
       "https://wcs.smartdraw.com/working-smarter/img/how-to-create-a-project-planning-map.svg?bn=15100111938",
+    gitHubUrl: "",
+    webUrl: "",
+    detailedInfo: {
+      myRole: "Role Example",
+      projectType: "Type Example",
+      sections: {
+        overview: {
+          objective: {
+            title: "Objective",
+            content:
+              "Objective Example",
+          },
+          targetAudience: {
+            title: "Target Audience",
+            content:
+              "Target audiente example",
+          },
+        },
+        design: {
+          wireframe: {
+            title: "Wireframe",
+            img: "",
+          },
+        },
+      },
+    },
   },
   {
     id: 3,
-    title: "Project",
+    title: "Project 3",
     desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
     skills: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      {
+        id: 100,
+        skillName: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      },
+      {
+        id: 101,
+        skillName: "Javascript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        id: 102,
+        skillName: "HTML",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      },
+      {
+        id: 103,
+        skillName: "Tailwind CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      },
+      {
+        id: 104,
+        skillName: "Vite",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+      },
     ],
     imgUrl:
-      "https://cdn.sanity.io/images/tlr8oxjg/production/d63904e726e42ae2491c29106065e9762dbcccde-1456x816.png?w=3840&q=80&fit=clip&auto=format",
+      "https://wcs.smartdraw.com/working-smarter/img/how-to-create-a-project-planning-map.svg?bn=15100111938",
+    gitHubUrl: "",
+    webUrl: "",
+    detailedInfo: {
+      myRole: "Role Example",
+      projectType: "Type Example",
+      sections: {
+        overview: {
+          objective: {
+            title: "Objective",
+            content:
+              "Objective Example",
+          },
+          targetAudience: {
+            title: "Target Audience",
+            content:
+              "Target audiente example",
+          },
+        },
+        design: {
+          wireframe: {
+            title: "Wireframe",
+            img: "",
+          },
+        },
+      },
+    },
   },
   {
     id: 4,
-    title: "Project",
+    title: "Project 4",
     desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
     skills: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      {
+        id: 100,
+        skillName: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      },
+      {
+        id: 101,
+        skillName: "Javascript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        id: 102,
+        skillName: "HTML",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      },
+      {
+        id: 103,
+        skillName: "Tailwind CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      },
+      {
+        id: 104,
+        skillName: "Vite",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+      },
     ],
     imgUrl:
-      "https://www.projectmanager.com/wp-content/uploads/2022/09/Project-Management.png",
+      "https://wcs.smartdraw.com/working-smarter/img/how-to-create-a-project-planning-map.svg?bn=15100111938",
+    gitHubUrl: "",
+    webUrl: "",
+    detailedInfo: {
+      myRole: "Role Example",
+      projectType: "Type Example",
+      sections: {
+        overview: {
+          objective: {
+            title: "Objective",
+            content:
+              "Objective Example",
+          },
+          targetAudience: {
+            title: "Target Audience",
+            content:
+              "Target audiente example",
+          },
+        },
+        design: {
+          wireframe: {
+            title: "Wireframe",
+            img: "",
+          },
+        },
+      },
+    },
   },
   {
     id: 5,
-    title: "Project",
+    title: "Project 5",
     desc: "This is an example project with fake images, the only objective is to show what a real project would look like.",
     skills: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      {
+        id: 100,
+        skillName: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      },
+      {
+        id: 101,
+        skillName: "Javascript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        id: 102,
+        skillName: "HTML",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      },
+      {
+        id: 103,
+        skillName: "Tailwind CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      },
+      {
+        id: 104,
+        skillName: "Vite",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+      },
     ],
     imgUrl:
-      "https://www.elmhurst.edu/wp-content/uploads/2018/12/5-skills-project-management-degree-elmhurst-college-infographic-thumb.jpg",
+      "https://wcs.smartdraw.com/working-smarter/img/how-to-create-a-project-planning-map.svg?bn=15100111938",
+    gitHubUrl: "",
+    webUrl: "",
+    detailedInfo: {
+      myRole: "Role Example",
+      projectType: "Type Example",
+      sections: {
+        overview: {
+          objective: {
+            title: "Objective",
+            content:
+              "Objective Example",
+          },
+          targetAudience: {
+            title: "Target Audience",
+            content:
+              "Target audiente example",
+          },
+        },
+        design: {
+          wireframe: {
+            title: "Wireframe",
+            img: "",
+          },
+        },
+      },
+    },
   },
 ];
 
