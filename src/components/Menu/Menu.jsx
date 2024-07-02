d2655e2e991743745566a35831b0e4d77d4ecbf4
Itamar <itamarpj00@hotmail.com>
2024-07-02 12:05:07 -0300

    feat: I updated the menu to use react-scroll and router-dom to scroll when the button path is on the page, or change the page and scroll to section when it isn't in the page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
-import { Link } from "react-scroll";
+import { Link, animateScroll as scroll } from "react-scroll";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Menu = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  // Size of menu to control the offset when scrolling
+  const menuSize = 64
+  
+  // Handle the menu navigation, if in project page it'll go back to home 
+  // and scroll to section, otherwise it'll just scroll to the section
+  const handleNavigation = (path, id) => {
+    if (location.pathname === '/') {
+      scroll.scrollTo(document.getElementById(id).offsetTop - menuSize)
+    } 
+    else {
+      navigate(path);
+      setTimeout(() => {
+        scroll.scrollTo(document.getElementById(id).offsetTop - menuSize)
+      }, 300); // Ajuste esse timeout conforme necessário
+    }
+    setIsOpen(false)
+  }
+
   // Manage the state of mobile menu
   const [isOpen, setIsOpen] = useState(false)
   const toggleMenu = () => {
     setIsOpen(!isOpen)
-  }
-  const closeMenu = () => {
-    setIsOpen(false)
   }
   // 
 
@@ -22,19 +41,18 @@ const Menu = () => {
         {/* Verify screen size to change to mobile/desktop version */}
         <nav className={`${isOpen ? "flex" : "hidden sm:!flex"} pb-8 bg-midnight absolute top-full left-0 flex-col w-full items-center sm:gap-3 sm:flex-row sm:relative sm:w-max sm:left-0 sm:pb-0 sm:bg-transparent`}>
           
-          <Link to="About" spy={true} smooth={true} duration={1000} offset={-64} onClick={closeMenu} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
-            About
-          </Link>
-          <Link to="Skills" spy={true} smooth={true} duration={1000} offset={-64} onClick={closeMenu} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
-            Skills  
-          </Link>
-          <Link to="Projects" spy={true} smooth={true} duration={1000} offset={-64} onClick={closeMenu} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
-            Projects
-          </Link>
-          <Link to="Contact" spy={true} smooth={true} duration={1000} offset={-64} onClick={closeMenu} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
-            Contact
-          </Link>
-          
+            <Link to="About" onClick={() => handleNavigation('/', 'About')} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+              About
+            </Link> 
+            <Link to="About" onClick={() => handleNavigation('/', 'Skills')} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+              Skills
+            </Link> 
+            <Link to="About" onClick={() => handleNavigation('/', 'Projects')} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+              Projects
+            </Link> 
+            <Link to="About" onClick={() => handleNavigation('/', 'Contact')} className="p-2 cursor-pointer border-b-2 border-blue-400 border- sm:border-b-transparent hover:border-b-blue-400 hover:text-blue-400">
+              Contact
+            </Link>
         </nav>
       </div>
     </div>
