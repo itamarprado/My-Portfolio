abdc93127c604728b6eeda832beca9d9d03ccbbd
praad0o <itamarpj00@hotmail.com>
2024-07-01 23:04:15 -0300

    refac: remove unused elements/components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
-import Menu from "./components/Menu/Menu"
-import Presentation from "./components/Presentation/Presentation"
-import Projects from "./components/Projects/Projects"
-import About from "./components/About/About"
-import Skills from "./components/Skills/Skills"
-import Footer from "./components/Footer/Footer"
-import Contact from "./components/Contact/Contact"
-
 const App = () => {
   return (
     <>
-      <Menu />
-      <Presentation />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
     </>
   )
 }
