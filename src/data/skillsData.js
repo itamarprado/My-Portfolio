8754e1252257163dc8d5de3aa51c482c42bd4452
Itamar <itamarpj00@hotmail.com>
2024-07-04 13:27:19 -0300

    refac: I moved the skills data to a new file called skillsData.js to reduce code verbosity

---
+++ src/data/skillsData.js
@@ -0,0 +1,77 @@
+const hardSkills = [
+  {
+    id: 1,
+    name: "HTML5",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+  },
+  {
+    id: 2,
+    name: "CSS3",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+  },
+  {
+    id: 3,
+    name: "Javascript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  },
+  {
+    id: 4,
+    name: "React",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+  },
+  {
+    id: 5,
+    name: "Tailwind CSS",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+  },
+  {
+    id: 6,
+    name: "Git",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+  },
+  {
+    id: 7,
+    name: "Github",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
+  },
+  {
+    id: 8,
+    name: "Databases",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg",
+  },
+  {
+    id: 9,
+    name: "Python",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+  },
+  {
+    id: 10,
+    name: "Angular",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
+  },
+];
+
+const softSkills = [
+  {
+    id: 11,
+    name: "Communication",
+    icon: "/icons/comm.png",
+  },
+  {
+    id: 12,
+    name: "Problem Solving",
+    icon: "/icons/problem-solving.png",
+  },
+  {
+    id: 13,
+    name: "Adaptability",
+    icon: "/icons/adaptability.png",
+  },
+  {
+    id: 14,
+    name: "Never-end learning",
+    icon: "/icons/never-end-learning.png",
+  },
+];
+
+export { hardSkills, softSkills };
